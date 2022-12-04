d74cac60e4475e2280861e87a15f4034aec39682
vAyu <114573223+vayu21@users.noreply.github.com>
2022-12-04 18:41:45 +0000

    Add files via upload

--- Blockchain.py
+++ Blockchain.py
@@ -1,65 +1,77 @@
 import random
 import datetime
+import time
 import hashlib as hashlibrary
+starting_time=time.time()
 #importing and reading json file
-print("----------------IMPORTING THE JSON FILE IN PYTHON--------------------")
+print("----------------JSON FILE IMPORTED IN PYTHON--------------------")
 import json as js
 jsonFilePath = r'F:\Uni of Liv\Introduction to Programming\Python Assignment\political-emails.json'
 with open(jsonFilePath) as email_file:
     email_data = js.load(email_file)
 #Checking and verifying data format
-print(type(email_data))
+"""print(type(email_data))
 print(email_data[0]) #to check first element of json file
 #to organise the email data in an much more organised and readable manner
 for i in email_data[0]:
     print (i,"-",email_data[0][i])
+"""
+A=input("Please enter 'A' or 'a' or '1' to display output of TASK A or Enter 'E' or 'e' to execute the whole programme: ")
 #------------------------------------------------------------------TASK A----------------------------------------------------------------------#
 print()
 print("--------------------------------------------------------------------------------------TASK A----------------------------------------------------------------------------------------")
 print()
-print(len(email_data))
+if A=='1'or A=='a'or A=='A' or A=='E' or A=='e':
+    print(len(email_data))
 """
 Since our data is classified as 'list' and each element is an email, so we use 'len' function to count the length of the list.
 Hence, the code above (line 13) gives out the total number of political emails in the json file
 """
 key_check=list(email_data[0].keys())
-print(key_check)
-for each_email in email_data:
-    if key_check!=list(each_email.keys()):
-        print("Keys are not identical")
-        break
-#To check if the above loop is fully executed till the end of the list and is not breaking in between. If not so, then the elements are identical.
-if email_data.index(each_email)==len(email_data)-1:
-    print("Keys are identical")
+if A=='1'or A=='a'or A=='A' or A=='E' or A=='e':
+    print(key_check)
+    for each_email in email_data:
+        if key_check!=list(each_email.keys()):
+            print("Keys are not identical")
+            break
+    #To check if the above loop is fully executed till the end of the list and is not breaking in between. If not so, then the elements are identical.
+    if email_data.index(each_email)==len(email_data)-1:
+        print("Keys are identical")
 """
 The above code is meant to compare keys of each element with the keys of the first element in the list. 
 It is to check whether the keys are identical or not. 
 """
 #-----------------------------------------------------------------TASK B----------------------------------------------------------------------#
 print()
+if A!='E' and A!='e':
+    B=input("Please enter 'B' or 'b' or '2' to display output of TASK B: ")
 print("--------------------------------------------------------------------------------------TASK B----------------------------------------------------------------------------------------")
 print()
-encoded_block = str(email_data[8]).encode()
-hashvalue1 = hashlibrary.sha256(encoded_block).hexdigest()
-print(hashvalue1)
+if A=='E' or A=='e' or B=='2'or B=='b'or B=='B':
+    encoded_block = str(email_data[8]).encode()
+    hashvalue1 = hashlibrary.sha256(encoded_block).hexdigest()
+    print(hashvalue1)
 
-encoded_block = str(email_data[8]).encode()
-hashvalue2 = hashlibrary.sha256(encoded_block).hexdigest()
-print(hashvalue2)
+    encoded_block = str(email_data[8]).encode()
+    hashvalue2 = hashlibrary.sha256(encoded_block).hexdigest()
+    print(hashvalue2)
 
-encoded_block = str(email_data[10]).encode()
-hashvalue3 = hashlibrary.sha256(encoded_block).hexdigest()
-print(hashvalue3)
+    encoded_block = str(email_data[10]).encode()
+    hashvalue3 = hashlibrary.sha256(encoded_block).hexdigest()
+    print(hashvalue3)
 
-#Checking the identicality of the hash values
-if hashvalue1==hashvalue2:
-    print("'hashvalue1' and 'hashvalue2' are identical")
-else: print("'hashvalue1' and 'hashvalue2' are not identical")
+    #Checking the identicality of the hash values
+    if hashvalue1==hashvalue2:
+        print("'hashvalue1' and 'hashvalue2' are identical")
+    else: print("'hashvalue1' and 'hashvalue2' are not identical")
 
-if hashvalue1==hashvalue3:
-    print("'hashvalue1' and 'hashvalue3' are identical")
-else: print ("'hashvalue1' and 'hashvalue3' are not identical")
+    if hashvalue1==hashvalue3:
+        print("'hashvalue1' and 'hashvalue3' are identical")
+    else: print ("'hashvalue1' and 'hashvalue3' are not identical")
+
 print()
+if A!='E' and A!='e':
+    C=input("Please enter 'C' or 'c' or '3' to display output of TASK C: ")
 print("--------------------------------------------------------------------------------------TASK C----------------------------------------------------------------------------------------")
 print()
 #------------------------------------------------------------------TASK C1------------------------------------------------------------------------#
@@ -129,6 +141,10 @@ for j in chain:
 for k in range(121):
     mine_block(random.randint(0,405))
 #to check if our blockchain is complete and working successfully
-for z in chain:
-    print(z)
+if A=='E' or A=='e' or C=='3'or C=='c'or C=='C':
+    for z in chain:
+        print(z)
+print()
+print(" Programme Duration: %s seconds   "% (time.time() - starting_time))
+print("---------------------------------------------------------------------------------THANK YOU----------------------------------------------------------------------------------")
 #--------------------------------------------------------------------*******-------------------------------------------------------------------#
